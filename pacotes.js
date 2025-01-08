e579ace7be97fd9abf42a6a8b3d8c0b2fdec029d
zolthan31 <dhb.tecnologia@outlook.com>
2025-01-08 13:10:45 -0300

    site 3.02

---
+++ pacotes.js
@@ -0,0 +1,139 @@
+document.getElementById('contactButton').addEventListener('click', () => {
+    const phone = "5591988074549";
+    const message = encodeURIComponent("Olá! Gostaria de informações sobre pacote de alfabetização.");
+    const whatsappUrl = `https://wa.me/${phone}?text=${message}`;
+    window.open(whatsappUrl, '_blank');
+  });
+  document.getElementById('contactButton2').addEventListener('click', () => {
+    const phone = "5591988074549";
+    const message = encodeURIComponent("Olá! Gostaria de informações sobre pacote de alfabetização.");
+    const whatsappUrl = `https://wa.me/${phone}?text=${message}`;
+    window.open(whatsappUrl, '_blank');
+  });
+  document.getElementById('contactButton3').addEventListener('click', () => {
+    const phone = "5591988074549";
+    const message = encodeURIComponent("Olá! Gostaria de informações sobre pacote de alfabetização.");
+    const whatsappUrl = `https://wa.me/${phone}?text=${message}`;
+    window.open(whatsappUrl, '_blank');
+  });
+
+  document.getElementById('contactButton4').addEventListener('click', () => {
+    const phone = "5591988074549";
+    const message = encodeURIComponent("Olá! Gostaria de informações sobre pacotes do 1° ao 3° ano.");
+    const whatsappUrl = `https://wa.me/${phone}?text=${message}`;
+    window.open(whatsappUrl, '_blank');
+  });
+  document.getElementById('contactButton5').addEventListener('click', () => {
+    const phone = "5591988074549";
+    const message = encodeURIComponent("Olá! Gostaria de informações sobre pacotes do 1° ao 3° ano.");
+    const whatsappUrl = `https://wa.me/${phone}?text=${message}`;
+    window.open(whatsappUrl, '_blank');
+  });
+  document.getElementById('contactButton6').addEventListener('click', () => {
+    const phone = "5591988074549";
+    const message = encodeURIComponent("Olá! Gostaria de informações sobre pacotes do 1° ao 3° ano.");
+    const whatsappUrl = `https://wa.me/${phone}?text=${message}`;
+    window.open(whatsappUrl, '_blank');
+  });
+
+  document.getElementById('contactButton7').addEventListener('click', () => {
+    const phone = "5591988074549";
+    const message = encodeURIComponent("Olá! Gostaria de informações sobre pacotes do 4° e 5° ano.");
+    const whatsappUrl = `https://wa.me/${phone}?text=${message}`;
+    window.open(whatsappUrl, '_blank');
+  });
+  document.getElementById('contactButton8').addEventListener('click', () => {
+    const phone = "5591988074549";
+    const message = encodeURIComponent("Olá! Gostaria de informações sobre pacotes do 4° e 5° ano.");
+    const whatsappUrl = `https://wa.me/${phone}?text=${message}`;
+    window.open(whatsappUrl, '_blank');
+  });
+  document.getElementById('contactButton9').addEventListener('click', () => {
+    const phone = "5591988074549";
+    const message = encodeURIComponent("Olá! Gostaria de informações sobre pacotes do 4° e 5° ano.");
+    const whatsappUrl = `https://wa.me/${phone}?text=${message}`;
+    window.open(whatsappUrl, '_blank');
+  });
+
+  document.getElementById('contactButton10').addEventListener('click', () => {
+    const phone = "5591988074549";
+    const message = encodeURIComponent("Olá! Gostaria de informações sobre pacotes do 6°ano.");
+    const whatsappUrl = `https://wa.me/${phone}?text=${message}`;
+    window.open(whatsappUrl, '_blank');
+  });
+  document.getElementById('contactButton11').addEventListener('click', () => {
+    const phone = "5591988074549";
+    const message = encodeURIComponent("Olá! Gostaria de informações sobre pacotes do 6°ano.");
+    const whatsappUrl = `https://wa.me/${phone}?text=${message}`;
+    window.open(whatsappUrl, '_blank');
+  });
+  document.getElementById('contactButton12').addEventListener('click', () => {
+    const phone = "5591988074549";
+    const message = encodeURIComponent("Olá! Gostaria de informações sobre pacotes do 6°ano.");
+    const whatsappUrl = `https://wa.me/${phone}?text=${message}`;
+    window.open(whatsappUrl, '_blank');
+  });
+
+  document.getElementById('contactButton13').addEventListener('click', () => {
+    const phone = "5591988074549";
+    const message = encodeURIComponent("Olá! Gostaria de informações sobre pacotes do 7°ano.");
+    const whatsappUrl = `https://wa.me/${phone}?text=${message}`;
+    window.open(whatsappUrl, '_blank');
+  });
+  document.getElementById('contactButton14').addEventListener('click', () => {
+    const phone = "5591988074549";
+    const message = encodeURIComponent("Olá! Gostaria de informações sobre pacotes do 7°ano.");
+    const whatsappUrl = `https://wa.me/${phone}?text=${message}`;
+    window.open(whatsappUrl, '_blank');
+  });
+  document.getElementById('contactButton15').addEventListener('click', () => {
+    const phone = "5591988074549";
+    const message = encodeURIComponent("Olá! Gostaria de informações sobre pacotes do 7°ano.");
+    const whatsappUrl = `https://wa.me/${phone}?text=${message}`;
+    window.open(whatsappUrl, '_blank');
+  });
+
+  document.getElementById('contactButton16').addEventListener('click', () => {
+    const phone = "5591988074549";
+    const message = encodeURIComponent("Olá! Gostaria de informações sobre pacotes do 8°ano.");
+    const whatsappUrl = `https://wa.me/${phone}?text=${message}`;
+    window.open(whatsappUrl, '_blank');
+  });
+  document.getElementById('contactButton17').addEventListener('click', () => {
+    const phone = "5591988074549";
+    const message = encodeURIComponent("Olá! Gostaria de informações sobre pacotes do 8°ano.");
+    const whatsappUrl = `https://wa.me/${phone}?text=${message}`;
+    window.open(whatsappUrl, '_blank');
+  });
+  document.getElementById('contactButton18').addEventListener('click', () => {
+    const phone = "5591988074549";
+    const message = encodeURIComponent("Olá! Gostaria de informações sobre pacotes do 8°ano.");
+    const whatsappUrl = `https://wa.me/${phone}?text=${message}`;
+    window.open(whatsappUrl, '_blank');
+  });
+
+  document.getElementById('contactButton19').addEventListener('click', () => {
+    const phone = "5591988074549";
+    const message = encodeURIComponent("Olá! Gostaria de informações sobre pacotes do 9°ano.");
+    const whatsappUrl = `https://wa.me/${phone}?text=${message}`;
+    window.open(whatsappUrl, '_blank');
+  });
+  document.getElementById('contactButton20').addEventListener('click', () => {
+    const phone = "5591988074549";
+    const message = encodeURIComponent("Olá! Gostaria de informações sobre pacotes do 9°ano.");
+    const whatsappUrl = `https://wa.me/${phone}?text=${message}`;
+    window.open(whatsappUrl, '_blank');
+  });
+  document.getElementById('contactButton21').addEventListener('click', () => {
+    const phone = "5591988074549";
+    const message = encodeURIComponent("Olá! Gostaria de informações sobre pacotes do 9°ano.");
+    const whatsappUrl = `https://wa.me/${phone}?text=${message}`;
+    window.open(whatsappUrl, '_blank');
+  });
+
+  document.getElementById('contactButton22').addEventListener('click', () => {
+    const phone = "5591988074549";
+    const message = encodeURIComponent("Olá! Gostaria de informações sobre pacote de hora aula.");
+    const whatsappUrl = `https://wa.me/${phone}?text=${message}`;
+    window.open(whatsappUrl, '_blank');
+  });
